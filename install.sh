9dd9c3c23113e8bca0346c3107086fb3b4781e9c
zannabianca1997 <zannabianca199712@gmail.com>
2020-09-07 13:32:04 +0200

    Added uninstall to install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,42 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 install_dir="/opt/PiHoleBot"
 conf_dir="/etc/opt/PiHoleBot"
 
-echo "Creating directories"
-mkdir $install_dir
-mkdir $conf_dir
+print_usage() {
+  printf "Usage: install [-u]"
+  printf "Install the bot on the system. Use the -u to uninstall"
+}
 
-echo "Copying source to directory"
-cp "$DIR/src/*" $install_dir/
+install() {
+  printf "Creating directories"
+  mkdir "$install_dir"
+  mkdir "$conf_dir"
 
-echo "Copying configuration files"
-mv "$DIR/conf" $conf_dir/
-touch $conf_dir/users.ids
-touch $conf_dir/admins.ids
+  printf "Copying scripts to %s" "$install_dir"
+  cp "$DIR/src/*" "$install_dir/"
 
-echo "Done."
+  printf "Copying configuration files to %s" "$conf_dir"
+  mv "$DIR/conf" "$conf_dir/"
+  touch "$conf_dir/users.ids"
+  touch "$conf_dir/admins.ids"
+
+  printf "Done."
+}
+
+uninstall(){
+  printf "Removing scripts from %s" "$install_dir"
+  rm -rf "$install_dir"
+
+  printf "Removing configuration files from %s" "$conf_dir"
+  mv "$DIR/conf" "$conf_dir/"
+}
+
+while getopts 'u' flag; do
+  case "${flag}" in
+    u) uninstall
+       exit 0 ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+install
 exit 0
