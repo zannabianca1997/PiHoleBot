e6433768efdd714af3551d4f3c64ff792f3153f9
zannabianca1997 <zannabianca199712@gmail.com>
2020-09-07 14:55:07 +0200

    Moved PiHoleBot to piholebot.py Renamed pihole.py to pihole_control.py Added logging number of users at startup

--- src/users.py
+++ src/users.py
@@ -1,7 +1,13 @@
+import logging
+
 from setup import user_ids, admin_ids
+
+logger = logging.getLogger(__name__)
 
 with open(user_ids) as user_file:
     users_list = [int(line) for line in user_file]
 
 with open(admin_ids) as admin_file:
     admins_list = [int(line) for line in admin_file]
+
+logger.info(f"Loaded {len(users_list)} users, of which {len(admins_list)} are administrators")
