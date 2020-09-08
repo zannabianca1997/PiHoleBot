2be6e8f5a92c7d846242b46290bbac70fe8e4267
zannabianca1997 <zannabianca199712@gmail.com>
2020-09-08 20:35:24 +0200

    Added possibility to add users

--- src/manage_user.py
+++ src/manage_user.py
@@ -7,7 +7,7 @@ from telepot.aio import Bot
 
 from setup import token_file
 from users import add_user, add_admin
-from users import admins_list
+from users import admins_list, users_list
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +15,35 @@ logger = logging.getLogger(__name__)
 async def first_setup(bot):
     """Guida l'utente all'aggiunta del primo user e admin"""
     logger.info("Starting first admin confirmation")
+    new_user_id = await find_user_id(bot)
+    add_user(new_user_id)
+    add_admin(new_user_id)
+    return new_user_id
+
+
+async def choose_admin(bot: Bot):
+    """Ask to send a password, checks if the sender is an admin"""
+    logger.info("Starting admin confirmation")
+    admin_id = await find_user_id(bot)
+    if admin_id not in admins_list:
+        user = (await bot.getChatMember(admin_id, admin_id))["user"]
+        logger.warning(f"User {user['first_name']} tried to confirm as a admin")
+        print("You are not in the admin list. Please make an admin insert you in it")
+        exit(1)
+    return admin_id
+
+
+async def find_user_id(bot):
+    """Ask to send a one-time password, then return the user_id of whoever sent it"""
     setup_token = secrets.token_urlsafe(5)  # 5 so it's a short string. it's single use anyway
     bot_properties = await bot.getMe()
     print(f"Please write to the bot at @{bot_properties['username']} in private chat.\n"
           f" The one time password is {setup_token}")
     new_user_id = None
+    offset = -1
     while new_user_id is None:
-        for update in await bot.getUpdates(allowed_updates=["message"]):  # collect all messages
+        for update in await bot.getUpdates(allowed_updates=["message"], offset=offset):  # collect all messages
+            offset = max(update['update_id'], offset + 1)  # set new offset to ignore old messages
             if "message" not in update:  # sometimes some old updates can get through
                 continue
             msg = update["message"]
@@ -37,8 +59,49 @@ async def first_setup(bot):
                 break  # no need to check other messages
     logger.info(f"Accepted token from {new_user_id}")
     print("Password received")
-    add_user(new_user_id)
-    add_admin(new_user_id)
+    return new_user_id
+
+
+async def edit_users(bot):
+    if len(admins_list) == 0:  # non c'è nessun admin
+        editing_admin = await first_setup(bot)
+    else:  # chiede di verificarsi
+        editing_admin = await choose_admin(bot)
+    user = (await bot.getChatMember(editing_admin, editing_admin))["user"]
+    logger.info(f"Admin {user['first_name']} ({editing_admin}) start editing users")
+    offset = -1
+    while not exiting:
+        await bot.sendMessage(editing_admin, "Mandami l'utente che vuoi aggiungere, /stop per uscire")
+        exiting, user_given, offset = await _get_user_or_exit(bot, editing_admin, offset)
+        if exiting:
+            break
+        if user_given in users_list:
+            await bot.sendMessage(editing_admin, "Utente già presente")
+        else:
+            await bot.sendMessage(editing_admin, "Aggiungo l'utente alla userlist")
+            add_user(user_given)
+
+
+async def _get_user_or_exit(bot, editing_admin, offset):
+    user_given = None
+    exiting = False
+    while (user_given is None) or exiting:
+        for update in await bot.getUpdates(allowed_updates=["message"], offset=offset):  # collect all messages
+            offset = max(update['update_id'], offset + 1)  # set new offset to ignore old messages
+            if "message" not in update:  # sometimes some old updates can get through
+                continue
+            msg = update["message"]
+            if "text" in msg and msg["text"].count("/stop") > 0:  # stop ricevuto
+                exiting = True
+            if "contact" in msg:
+                contact = msg["contact"]
+                if "user_id" not in contact:
+                    await bot.sendMessage(editing_admin,
+                                          "Questo contatto non è su Telegram",
+                                          reply_to_message_id=msg["message_id"])
+                else:
+                    user_given = contact["user_id"]
+    return exiting, user_given, offset
 
 
 if __name__ == "__main__":
@@ -51,6 +114,4 @@ if __name__ == "__main__":
     with open(token_file) as tkn_file:
         TOKEN = tkn_file.read()
     bot = Bot(TOKEN)
-    if len(admins_list) == 0:  # nessun admin, primo setup
-        loop.run_until_complete(first_setup(bot))
-    # todo: create user add/remove bot
+    loop.run_until_complete(edit_users(bot))
